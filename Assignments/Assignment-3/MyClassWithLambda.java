d9643c4d6e288783cf7007e2de5b1001943e4ce9
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-15 07:58:17 +0530

    Add MyClassWithLambda.java

---
+++ Assignments/Assignment-3/MyClassWithLambda.java
@@ -0,0 +1,9 @@
+public class MyClassWithLambda {
+    public static void main(String[] args) {
+        WordCount wc = str -> {
+            return (str.split(" ").length);
+        };
+
+        System.out.println(wc.count("This is a test"));
+    }
+}
