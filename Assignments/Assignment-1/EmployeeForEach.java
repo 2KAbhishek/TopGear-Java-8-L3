8dcf63d0f4d91cd739eab795b50528003e452c7e
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-10-15 07:58:04 +0530

    Add EmployeeForEach.java

---
+++ Assignments/Assignment-1/EmployeeForEach.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class EmployeeForEach {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee(1, 100.0, "James", "Kolkata");
+        Employee emp2 = new Employee(2, 200.0, "Kames", "Colkata");
+        Employee emp3 = new Employee(3, 300.0, "Lames", "Lolkata");
+        Employee emp4 = new Employee(4, 400.0, "Names", "Tolkata");
+        Employee emp5 = new Employee(5, 500.0, "Pames", "Bolkata");
+
+        ArrayList<Employee> empList = new ArrayList<Employee>();
+        empList.add(emp1);
+        empList.add(emp2);
+        empList.add(emp3);
+        empList.add(emp4);
+        empList.add(emp5);
+
+        empList.forEach(emp -> System.out.println(
+                "ID : " + emp.id + " Name : " + emp.name + " Salary : " + emp.sal + " Address : " + emp.address));
+    }
+
+}
